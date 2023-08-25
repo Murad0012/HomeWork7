023d30b9c78a3adfb4f4f4c998994d8347896657
Predator <murad.mammedzade11@gmail.com>
2023-08-25 02:41:06 -0700

    First save

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Date = Console.ReadLine();
+            try
+            {
+                DateTime vaxt = DateTime.ParseExact(Date, "dd | MM | yyyy", null);
+                Console.WriteLine(vaxt.ToString("dd | MM | yyyy"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
